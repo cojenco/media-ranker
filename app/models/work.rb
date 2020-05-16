626fa235f13a518f39a8e5be9e912a7597352eb0
cojenco <icathyouyang@gmail.com>
2020-05-16 14:40:05 -0700

    add Work#max_votes custom method to model and revise homepage view

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,4 +5,8 @@ class Work < ApplicationRecord
   def self.top_ten(category)
     return Work.where(category: category).max_by(10) {|work| work.votes.count}
   end
+
+  def self.max_votes
+    return Work.all.max_by {|work| work.votes.count}
+  end
 end
