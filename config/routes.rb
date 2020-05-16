5a9b98358845d005897762883aa69bf76bf58fe1
cojenco <icathyouyang@gmail.com>
2020-05-16 11:08:56 -0700

    add nested upvote path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "works#index"
-  resources :works
+  resources :works do
+    post "/upvote", to: "votes#create", as: "upvote"
+  end
   resources :users, only: [:index, :show]
 
 end
