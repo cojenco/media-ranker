8f77d06fb052ebd9540d0b528e2e0c69c5e39ad7
cojenco <icathyouyang@gmail.com>
2020-05-16 00:46:28 -0700

    add works#update works#edit actions and views

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -12,26 +12,50 @@ class WorksController < ApplicationController
     end
   end
 
+
   def new
     @work = Work.new
   end
+
 
   def create
     @work = Work.new(work_params)
 
     if @work.save
+      flash[:success] = "Successfully created #{@work.category} #{@work.id}"
       redirect_to work_path(@work.id)
       return
     else
+      flash.now[:error] = "Ooops! Could not create new work."
       render :new, status: :bad_request
       return
     end
   end
 
+
   def edit
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 
+
   def update
+    @work = Work.find_by(id: params[:id])
+    return head :not_found if @work.nil?
+
+    if @work.update(work_params)
+      flash[:success] = "Successfully updated #{@work.category} #{@work.id}"
+      redirect_to work_path(@work.id)
+      return
+    else
+      flash.now[:error] = "Ooops! #{@work.category} #{@work.id} was not updated."
+      render :edit, status: :bad_request
+      return
+    end
   end
 
   def destroy
