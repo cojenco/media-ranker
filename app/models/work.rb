da0216273680a2b1bebd931b6f0b98a9ae28b294
cojenco <icathyouyang@gmail.com>
2020-05-16 15:11:42 -0700

    add validation to Work and corresponding views

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,6 +2,8 @@ class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
 
+  validates :title, presence: true
+
   def self.top_ten(category)
     return Work.where(category: category).max_by(10) {|work| work.votes.count}
   end
