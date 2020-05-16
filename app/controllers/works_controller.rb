c1778bf9687e2f0f86dd502826e00ef8b4244e01
cojenco <icathyouyang@gmail.com>
2020-05-15 23:25:29 -0700

    add seeds.rb to help load csv with db seeding

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,20 +1,21 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def show
+  end
+
   def new
   end
 
   def create
   end
 
-  def update
-  end
-
   def edit
   end
 
-  def index
-  end
-
-  def show
+  def update
   end
 
   def destroy
