c134117bab3ce2d00731388b682c63570e84111c
cojenco <icathyouyang@gmail.com>
2020-05-15 23:43:44 -0700

    add works#show action and view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,6 +4,12 @@ class WorksController < ApplicationController
   end
 
   def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 
   def new
