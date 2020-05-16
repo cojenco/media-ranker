12801463fbb80ac5ed004c73647094a3ce907f75
cojenco <icathyouyang@gmail.com>
2020-05-15 22:43:55 -0700

    generate work controller and view files

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,22 @@
+class WorksController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
