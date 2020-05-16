c46aba9e46f63ea33240cd50517326fc6e38cef8
cojenco <icathyouyang@gmail.com>
2020-05-16 10:36:47 -0700

    add resources routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "works#index"
   resources :works
-
+  resources :users, only: [:index, :show]
 
 end
