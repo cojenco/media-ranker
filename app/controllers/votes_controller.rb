5748e0486473780e3acb9935ec0d74844e4f68c5
cojenco <icathyouyang@gmail.com>
2020-05-16 11:58:57 -0700

    db migration to relate votes to users

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,19 @@
 class VotesController < ApplicationController
+  
+  def create
+    work_upvote = params[:work_id]
+    user_upvote = 1
+
+    @vote = Vote.new(work_id: work_upvote, user_id: user_upvote)
+    if @vote.save
+      flash[:success] = "Successfully upvoted!"
+      redirect_to work_path(work_upvote)
+      return
+    else
+      flash.now[:error] = "Ooops! Failed to upvote."
+      redirect_to work_path(work_upvote)
+      return
+    end
+
+  end
 end
