2ac879e433e0c4f578aa1218edfe9d39e97b49a2
cojenco <icathyouyang@gmail.com>
2020-05-16 14:17:20 -0700

    add Work#top_ten custom method to model and revise homepage view

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,8 @@
 class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
+
+  def self.top_ten(category)
+    return Work.where(category: category).max_by(10) {|work| work.votes.count}
+  end
 end
