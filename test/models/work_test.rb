6df0bf17780ed623b4c08eb538aa14b3c49c8662
cojenco <icathyouyang@gmail.com>
2020-05-17 11:27:48 -0700

    add work model tests for validations

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe "validations" do
+    before do
+      @work = Work.new(title: "Casablanca")
+    end
+
+    it "is valid as long as the title is present" do
+      expect(@work.valid?).must_equal true
+    end
+
+    it "is invalid without a title" do
+      invalid_work = works(:toystory)
+      invalid_work.title = nil
+      expect(invalid_work.valid?).must_equal false
+    end
+
+    it "is invalid with a non-unique title in the same category" do
+      @work.title = "Toy Story"
+      @work.category = :movie
+      expect(@work.valid?).must_equal false
+    end
+
+    it "is valid with a non-unique title in a different category" do
+      adaptation = works(:prince)
+      adaptation.category = :movie
+      expect(adaptation.valid?).must_equal true
+    end
+  end 
+
+  describe "relations" do
+    it "can have many votes" do
+    end
+
+    it "can have many users through votes" do
+    end
+  end
 end
