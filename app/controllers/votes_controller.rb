2927b7ce14d65053058293849313de1063bfe490
cojenco <icathyouyang@gmail.com>
2020-05-17 17:32:48 -0700

    add login session logic to votes#create

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,23 @@
 class VotesController < ApplicationController
   
   def create
-    work_upvote = params[:work_id].to_i
-    user_upvote = 1
-
-    @vote = Vote.new(work_id: work_upvote, user_id: user_upvote)
-    if @vote.save
-      flash[:success] = "Successfully upvoted!"
-      redirect_to work_path(work_upvote)
-      return
-    else
-      flash[:error] = "Ooops! Failed to upvote."
-      redirect_to work_path(work_upvote)
+    user = User.find_by(id: session[:user_id])
+    if user.nil?
+      flash[:warning] = "A problem occurred: You must log in to do that"
+      redirect_back(fallback_location: root_path)
       return
     end
 
+    upvote_work_id = params[:work_id].to_i
+    vote = Vote.new(work_id: upvote_work_id, user_id: user.id)
+    if vote.save
+      flash[:success] = "Successfully upvoted!"
+      redirect_to work_path(upvote_work_id)
+      return
+    else
+      flash[:error] = "Ooops! Failed to upvote."
+      redirect_to work_path(upvote_work_id)
+      return
+    end
   end
 end
