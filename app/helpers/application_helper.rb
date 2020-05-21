b31b47e718d2ff1e69eb3aea9c9061d18ffcf2d2
cojenco <icathyouyang@gmail.com>
2020-05-20 21:19:19 -0700

    practice using view helper methods #readable_date in works#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def readable_date(date)
+    if date.nil?
+      return ""
+    end
+
+    return content_tag(:span, "#{date.strftime("%b %d, %Y")}", class: "date", title: date.to_s )
+  end
 end
